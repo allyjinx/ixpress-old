1c94bb785464fda7f25072d62e68898877822bc1
shayna93 <shaynafoll93@gmail.com>
2019-01-02 20:55:50 -0500

    Added js
    + routing from page 0 -> page 1

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+window.onload = function(){ 
+
+	alert( 'Hello, world!' );
+
+	document.getElementById("myButton").onclick = function () {
+        location.href = "ixpress.page1.html";
+        };
+     };
