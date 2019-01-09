e73f7f21f6e1f027d49ccf74b44c6311c7cabd35
shayna93 <shaynafoll93@gmail.com>
2019-01-09 18:40:49 -0500

    ons-router

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,16 @@
-window.onload = function(){ 
 
-	alert( 'Hello, world!' );
 
-	document.getElementById("myButton").onclick = function () {
-        location.href = "ixpress.page1.html";
-        };
-     };
+document.addEventListener('init', function(event) {
+  var page = event.target;
+
+  if (page.id === 'page0') {
+    page.querySelector('#push-button').onclick = function() {
+      document.querySelector('#myNavigator').pushPage('ixpress.newuser.html');
+    };
+  } else if (page.id === 'newuser') {
+    page.querySelector('ons-button').onclick = function() {
+    	document.querySelector('#myNavigator').pushPage('ixpress.userlogin.html');
+    }
+  
+}
+});
